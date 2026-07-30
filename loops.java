b9a252a19402a0809c90a443442a95abbe656b4e
tejaschandankar <tejaschandankar58@gmail.com>
2026-07-31 00:03:50 +0530

    Update class files

--- loops.java
+++ loops.java
@@ -16,12 +16,22 @@
 
 //     }
 // }
+// class loops{
+//     public static void main(String[]args){
+//        int i=0;
+//        while(i<11){
+//             System.out.print(i+" ");
+//             i++;
+//         }
+//     }
+// }
+
 class loops{
     public static void main(String[]args){
-       int i=0;
-       while(i<11){
-            System.out.print(i+" ");
+        int i=0;
+        do{
+            System.out.println(i);
             i++;
-        }
+        }while(i<11);
     }
 }
