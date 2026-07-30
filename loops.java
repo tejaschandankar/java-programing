e7c7c3791a084b7d20b5a6d339bf15b833271eac
tejaschandankar <tejaschandankar58@gmail.com>
2026-07-31 00:20:07 +0530

    Update class files

--- loops.java
+++ loops.java
@@ -26,12 +26,41 @@
 //     }
 // }
 
-class loops{
+// class loops{
+//     public static void main(String[]args){
+//         int i=0;
+//         do{
+//             System.out.println(i);
+//             i++;
+//         }while(i<11);
+//     }
+// }
+
+// import java.util.*;
+// public class loops{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter the number:");
+//         int n=sc.nextInt();
+
+//         int sum=0;
+//         for(int i=1;i<=n;i++){
+//             sum=sum+i;
+//         }
+//         System.out.println(sum);    
+//     }
+// }
+
+import java.util.*;
+public class loops{
     public static void main(String[]args){
-        int i=0;
-        do{
-            System.out.println(i);
-            i++;
-        }while(i<11);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+        
+        for(int i=1;i<11;i++){
+            System.out.println(i*n);
+        }
+    
     }
 }
